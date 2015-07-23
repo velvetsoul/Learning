8f51b23cb4a5fdf355fcd6a860ca5af87c240a8b
velvetsoul <evgenij.arhipov@gmal.com>
2015-07-23 13:39:02 +0300

    1st part

---
+++ LearningС_ConsoleApps/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningС_ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
