f60649f6bf402b310339575b2e5947f2c12fc10c
velvetsoul <evgenij.arhipov@gmal.com>
2015-07-27 16:06:31 +0300

    27.07.15 возня с классами

---
+++ SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Motorcycle
+    {
+        public int driverIntensity;
+        public void PopAWheely()
+        {
+            for (int i = 0; i <= driverIntensity; i++)
+            {
+                Console.WriteLine("whooohoo");
+            }
+        }
+        public Motorcycle()
+        { }
+        /*public Motorcycle(int intensity)
+        {
+            driverIntensity = intensity;
+        }*/
+        public string name;
+        public void SetDriverName (string name)
+        {
+            this.name = name;
+        }
+        
+        public string driverName;
+        
+        public Motorcycle(int intensity)
+        {
+            SetIntensity(intensity);
+        }
+        public Motorcycle (int intensity, string name)
+        {
+            SetIntensity(intensity);
+            driverName = name;
+        }
+        public void SetIntensity (int intensity)
+        {
+            if (intensity>10)
+            { 
+                intensity = 10; 
+            }
+            driverIntensity = intensity;
+        }
+    }
+}
+// безумие како-то стр.190 троелсена, разобраться с this
